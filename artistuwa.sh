b070106ac071933a71df0949ebe21a7a01d2751a
tadesua <oluwatosin.adesua@gmail.com>
2026-09-21 17:39:47 -0400

    Update disk image to Debian 12 for VM instance

--- artistuwa.sh
+++ artistuwa.sh
@@ -57,7 +57,7 @@ gcloud compute instances create bigquery-instance \
     --provisioning-model=STANDARD \
     --service-account=bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/bigquery,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-    --create-disk=auto-delete=yes,boot=yes,device-name=bigquery-instance,image=projects/debian-cloud/global/images/debian-11-bullseye-v20240815,mode=rw,size=10,type=pd-balanced \
+    --create-disk=auto-delete=yes,boot=yes,device-name=bigquery-instance,image=projects/debian-cloud/global/images/debian-12-bookworm-v20260915,mode=rw,size=10,type=pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
